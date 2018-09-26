934eebef857a1053b97a8de88e34078cb1878cad
Gautam <gautamdasika@gmail.com>
2018-09-25 22:49:58 -0700

    get card deck

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-
+deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
 @app.route("/")
 def hello():
     return "Hello Azure!"
+
+@app.route("/getcards")
+def hello():
+    return [card for card in deck]
